36c521100fe17f9ac8f1ae7e9f14dedf64c16d3c
Ryan Bas <krados@gmail.com>
2020-06-07 00:16:37 -0400

    Add entry 4

--- src/reload_extension/chrome/background.js
+++ src/reload_extension/chrome/background.js
@@ -1,18 +1,38 @@
-chrome.webNavigation.onCompleted.addListener(function() {
-    var hostname = "io.github.ryanbas.reload_extension";
-    var port = null;
+function saveConnectedState(state) {
+    chrome.storage.local.set({
+        connected: state
+    });
+}
 
+chrome.runtime.onInstalled.addListener(function() {
+    saveConnectedState(false);
+})
+
+chrome.webNavigation.onCompleted.addListener(function() {
     function onNativeMessage(message) {
         var stringJson = JSON.stringify(message);
         console.log("Received " + stringJson)
+        chrome.tabs.query({active: true, currentWindow: true, url: 'file://*'}, function(tabs) {
+            if (tabs.length > 0) {
+                chrome.tabs.reload(tabs[0].id);
+            }
+        });
     }
 
     function onDisconnect(message) {
-        console.log("Failed to connect: " + chrome.runtime.lastError.message);
-        port = null;
+        console.log("Disconnected: " + chrome.runtime.lastError.message);
+        saveConnectedState(false);
     }
 
-    port = chrome.runtime.connectNative(hostname);
-    port.onMessage.addListener(onNativeMessage);
-    port.onDisconnect.addListener(onDisconnect);
+    chrome.storage.local.get(['connected', 'port'], function(result) {
+        let hostname = "io.github.ryanbas.reload_extension";
+        let connected = result.connected;
+
+        if (!connected) {
+            var port = chrome.runtime.connectNative(hostname);
+            port.onMessage.addListener(onNativeMessage);
+            port.onDisconnect.addListener(onDisconnect);
+            saveConnectedState(true);
+        }
+    });
 }, {url: [{urlMatches: "file://*"}]});
