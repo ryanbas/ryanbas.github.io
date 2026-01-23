102c2d201fdb79733992db7fcd33458c12a91860
Ryan Bas <krados@gmail.com>
2026-01-23 18:32:54 -0500

    first crack at storage module

---
+++ _test/storage-wrapper/spec.mjs
@@ -0,0 +1,204 @@
+import StorageWrapper from "../../js/module/storage-wrapper.mjs";
+
+describe("StorageWrapper", () => {
+    describe("construction", function() {
+        it("should require a namespace", () => {
+            expect(() => new StorageWrapper()).toThrowError(Error, "namespace is required");
+            expect(() => new StorageWrapper("")).toThrowError(Error, "namespace is required");
+            expect(() => new StorageWrapper(null)).toThrowError(Error, "namespace is required");
+            expect(() => new StorageWrapper(undefined)).toThrowError(Error, "namespace is required");
+        });
+
+        it("should require namespace to be typeof 'string'", () => {
+            expect(() => new StorageWrapper(1)).toThrowError(TypeError, "namespace must be typeof 'string'");
+            expect(() => new StorageWrapper(1.5)).toThrowError(TypeError, "namespace must be typeof 'string'");
+            expect(() => new StorageWrapper([])).toThrowError(TypeError, "namespace must be typeof 'string'");
+            expect(() => new StorageWrapper({})).toThrowError(TypeError, "namespace must be typeof 'string'");
+            expect(() => new StorageWrapper(true)).toThrowError(TypeError, "namespace must be typeof 'string'");
+        });
+
+        it("should store the namespace", () => {
+            expect(new StorageWrapper("namespace-stored").namespace).toBe("namespace-stored");
+        });
+
+        it("should allow an optional version", () => {
+            const underTest = new StorageWrapper("optional-version-test");
+
+            expect(underTest.version).not.toBe(jasmine.anything());
+        });
+
+        it("should require the version to be an Integer", () => {
+            expect(() => new StorageWrapper("version-is-int-test", "1")).toThrowError(TypeError, "version must be integer");
+            expect(() => new StorageWrapper("version-is-int-test", [])).toThrowError(TypeError, "version must be integer");
+            expect(() => new StorageWrapper("version-is-int-test", {})).toThrowError(TypeError, "version must be integer");
+            expect(() => new StorageWrapper("version-is-int-test", true)).toThrowError(TypeError, "version must be integer");
+        });
+
+        it("should store the version", () => {
+            const underTest = new StorageWrapper("version-stored", 5);
+
+            expect(underTest.version).toBe(5);
+        });
+    });
+
+    beforeEach(function () {
+        this.storage = {};
+        this.mockLocalStorage = {
+            setItem: (key, value) => this.storage[String(key)] = String(value),
+            getItem: (key) => this.storage[String(key)]
+        };
+    });
+
+    describe("save(key, value)", function() {
+        it("should save string value to storage with the namespace prepended to the key", function () {
+            const underTest = new StorageWrapper("save-test").withStorage(this.mockLocalStorage);
+            underTest.save("test-key", "test-value");
+
+            expect(this.storage).toHaveSize(1);
+            expect(this.storage["save-test:test-key"]).toBe("test-value");
+        });
+
+        it("should only allow string keys to be saved", function() {
+            const underTest = new StorageWrapper("save-only-allows-string-keys").withStorage(this.mockLocalStorage);
+            expect(() => underTest.save(1, "")).toThrowError(TypeError, "keys must be typeof 'string'");
+            expect(() => underTest.save(1.5, "")).toThrowError(TypeError, "keys must be typeof 'string'");
+            expect(() => underTest.save([], "")).toThrowError(TypeError, "keys must be typeof 'string'");
+            expect(() => underTest.save({}, "")).toThrowError(TypeError, "keys must be typeof 'string'");
+            expect(() => underTest.save(true, "")).toThrowError(TypeError, "keys must be typeof 'string'");
+        });
+
+        it("should not allow saving directly to storage-wrapper-obj-version", function() {
+            const underTest = new StorageWrapper("cant-save-storage-wrapper-obj-version").withStorage(this.mockLocalStorage);
+            expect(() => underTest.save("storage-wrapper-obj-version", 1)).toThrowError(Error, "storage-wrapper-obj-version is special key and cannot be saved");
+            expect(this.storage).toHaveSize(0);
+        });
+
+        it("should save the version as storage-wrapper-obj-version to storage if the version exists", function() {
+            const underTest = new StorageWrapper("version-saved-test", 5).withStorage(this.mockLocalStorage);
+            underTest.save("key", "value");
+
+            expect(this.storage).toHaveSize(2);
+            expect(this.storage["version-saved-test:key"]).toBe("value");
+            expect(this.storage["version-saved-test:storage-wrapper-obj-version"]).toBe("5");
+        });
+    });
+
+    describe("load(key)", function() {
+        it("should get a string value using a key with its namespace", function () {
+            this.storage["load-test:test-key"] = "test-value";
+            this.storage["test-key"] = "wrong key!";
+            const underTest = new StorageWrapper("load-test").withStorage(this.mockLocalStorage);
+
+            expect(underTest.load("test-key")).toBe("test-value");
+        });
+    });
+
+    describe("migrateToCurrentVersion(versionHistory, migrations)", function() {
+        it("should require that versionHistory is a non-empty array of Integer values with size at least 1", () => {
+            const underTest = new StorageWrapper("version-history-array-of-ints", 1);
+            expect(() => underTest.migrateToCurrentVersion()).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion("")).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion(null)).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion(undefined)).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion(1)).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion(1.5)).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion({})).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion(true)).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion([])).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+
+            expect(() => underTest.migrateToCurrentVersion([1, ""])).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion([1, null])).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion([1, undefined])).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion([1, 1.5])).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion([1, {}])).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+            expect(() => underTest.migrateToCurrentVersion([1, true])).toThrowError(TypeError, "versionHistory must be a non-empty array of Integer values");
+        });
+
+        it("should require that version is set", () => {
+            const underTest = new StorageWrapper("use-migration-path-requires-version");
+            
+            expect(() => underTest.migrateToCurrentVersion([1])).toThrowError(Error, "version must be set to use a migration path");
+        });
+
+        it("should require migrations to be an array when provided", () => {
+            const underTest = new StorageWrapper("version-history-array-of-ints", 2);
+
+            expect(() => underTest.migrateToCurrentVersion([1], 1)).toThrowError(TypeError, "migrations should be an array with objects each containing a fromVersion Integer, toVersion Integer, and migrate function");
+            expect(() => underTest.migrateToCurrentVersion([1], 1.5)).toThrowError(TypeError, "migrations should be an array with objects each containing a fromVersion Integer, toVersion Integer, and migrate function");
+            expect(() => underTest.migrateToCurrentVersion([1], {})).toThrowError(TypeError, "migrations should be an array with objects each containing a fromVersion Integer, toVersion Integer, and migrate function");
+            expect(() => underTest.migrateToCurrentVersion([1], true)).toThrowError(TypeError, "migrations should be an array with objects each containing a fromVersion Integer, toVersion Integer, and migrate function");
+        });
+
+        it("should require migrations to be an array with objects each containing a fromVersion Integer, toVersion Integer, and migrate function", () => {
+            const underTest = new StorageWrapper("version-history-array-of-ints", 2);
+            const valid = {
+                fromVersion: 1,
+                toVersion: 2,
+                migrate: () => null
+            };
+
+            const badFromVersion = Object.assign({}, valid);
+            badFromVersion.fromVersion = "1";
+            expect(() => underTest.migrateToCurrentVersion([1, 2], [badFromVersion])).toThrowError(TypeError, "migrations should be an array with objects each containing a fromVersion Integer, toVersion Integer, and migrate function");
+
+            const badToVersion = Object.assign({}, valid);
+            badToVersion.toVersion = "1";
+            expect(() => underTest.migrateToCurrentVersion([1, 2], [badToVersion])).toThrowError(TypeError, "migrations should be an array with objects each containing a fromVersion Integer, toVersion Integer, and migrate function");
+
+            const badMigrate = Object.assign({}, valid);
+            badMigrate.migrate = "1";
+            expect(() => underTest.migrateToCurrentVersion([1, 2], [badMigrate])).toThrowError(TypeError, "migrations should be an array with objects each containing a fromVersion Integer, toVersion Integer, and migrate function");
+        });
+
+        it("should run a single migration if version does not match", function() {
+            this.storage["migrate-test:storage-wrapper-obj-version"] = "1";
+            this.storage["migrate-test:old-user"] = "testuser";
+
+            const migration = {
+                fromVersion: 1,
+                toVersion: 2,
+                migrate: (storageWrapper) => {
+                    const oldUser = storageWrapper.load("old-user");
+                    storageWrapper.save("migrated-user", oldUser + "-migrated");
+                }
+            }
+
+            new StorageWrapper("migrate-test", 2).withStorage(this.mockLocalStorage).migrateToCurrentVersion([1], [migration]);
+
+            expect(this.storage).toHaveSize(3);
+            expect(this.storage["migrate-test:storage-wrapper-obj-version"]).toBe("2");
+            expect(this.storage["migrate-test:old-user"]).toBe("testuser");
+            expect(this.storage["migrate-test:migrated-user"]).toBe("testuser-migrated");
+        });
+
+        it("should run multiple migrations if required", function() {
+            this.storage["migrate-test:storage-wrapper-obj-version"] = "1";
+            this.storage["migrate-test:v1-user"] = "testuser";
+
+            const migrations = [{
+                fromVersion: 1,
+                toVersion: 2,
+                migrate: (storageWrapper) => {
+                    const oldUser = storageWrapper.load("v1-user");
+                    storageWrapper.save("v2-user", oldUser + "-migrated-v2");
+                }
+            }, {
+                fromVersion: 2,
+                toVersion: 3,
+                migrate: (storageWrapper) => {
+                    console.log("migrating");
+                    const oldUser = storageWrapper.load("v2-user");
+                    storageWrapper.save("v3-user", oldUser + "-migrated-v3");
+                }
+            }]
+
+            new StorageWrapper("migrate-test", 3).withStorage(this.mockLocalStorage).migrateToCurrentVersion([1, 2], migrations);
+
+            expect(this.storage).toHaveSize(4);
+            expect(this.storage["migrate-test:storage-wrapper-obj-version"]).toBe("3");
+            expect(this.storage["migrate-test:v1-user"]).toBe("testuser");
+            expect(this.storage["migrate-test:v2-user"]).toBe("testuser-migrated-v2");
+            expect(this.storage["migrate-test:v3-user"]).toBe("testuser-migrated-v2-migrated-v3");
+        });
+    });
+});
---
+++ js/module/storage-wrapper.mjs
@@ -0,0 +1,119 @@
+export default class StorageWrapper {
+    #INTERNAL_VERSION_KEY = "storage-wrapper-obj-version";
+    #namespace;
+    #storage;
+    #version;
+
+    constructor(namespace, version) {
+        if (!namespace) {
+            throw new Error("namespace is required");
+        }
+
+        if (typeof namespace !== "string") {
+            throw new TypeError("namespace must be typeof 'string'");
+        }
+
+        if (version && !Number.isInteger(version)) {
+            throw new TypeError("version must be integer");
+        }
+
+        this.#namespace = namespace;
+        this.#storage = window.localStorage;
+        this.#version = version;
+    }
+
+    migrateToCurrentVersion(versionHistory, migrations) {
+        if (!this.#version) {
+            throw new Error("version must be set to use a migration path");
+        } else if (!Array.isArray(versionHistory) || versionHistory.length === 0 || versionHistory.some(v => !Number.isInteger(v))) {
+            throw new TypeError("versionHistory must be a non-empty array of Integer values");
+        }
+
+        if (migrations) {
+            if (!Array.isArray(migrations)) {
+                throw new TypeError("migrations should be an array with objects each containing a fromVersion Integer, toVersion Integer, and migrate function");
+            } else if (migrations.some(m => !Number.isInteger(m.fromVersion) || !Number.isInteger(m.toVersion) || typeof m.migrate !== "function")) {
+                throw new TypeError("migrations should be an array with objects each containing a fromVersion Integer, toVersion Integer, and migrate function");
+            }
+        }
+
+        function findMigration(from, to) {
+            console.log("from is " + typeof from);
+            console.log("from is " + typeof to);
+            console.log("looking for migration from " + from + " to " + to);
+            const found = migrations.find(m => m.fromVersion === from && m.toVersion === to);
+            console.log("found " + found);
+            return found;
+        }
+
+        const storageVersion = this.#getStorageVersion();
+        if (storageVersion !== this.#version) {
+            const directMigration = findMigration(storageVersion, this.#version);
+            if (directMigration) {
+                directMigration.migrate(this);
+            } else {
+                for (let i = 0; i < migrations.length; i += 1) {
+                    for (let j = i + 1; j < migrations.length; j += 1) {
+                        const from = versionHistory[i];
+                        const to = versionHistory[j];
+                        
+                        findMigration(from, to)?.migrate(this);
+                    }
+                }
+
+                const previousVersion = versionHistory.at(-1);
+                findMigration(previousVersion, this.#version)?.migrate(this);
+            }
+        }
+
+        return this;
+    }
+
+    withStorage(storage) {
+        this.#storage = storage;
+        return this;
+    }
+
+    save(key, value) {
+        if (typeof key !== "string") {
+            throw new TypeError("keys must be typeof 'string'");
+        }
+
+        if (key === this.#INTERNAL_VERSION_KEY) {
+            throw new Error(this.#INTERNAL_VERSION_KEY + " is special key and cannot be saved");
+        }
+
+        this.#saveVersion();
+        this.#setItemWithNamespace(key, value);
+    }
+
+    load(key) {
+        return this.#getItemWithNamespace(key);
+    }
+
+    #saveVersion() {
+        if (this.#version) {
+            this.#setItemWithNamespace(this.#INTERNAL_VERSION_KEY, this.#version);
+        }
+    }
+
+    #setItemWithNamespace(key, value) {
+        this.#storage.setItem(this.#namespace + ":" + key, value);
+    }
+
+    #getItemWithNamespace(key) {
+        return this.#storage.getItem(this.#namespace + ":" + key);
+    }
+
+    #getStorageVersion() {
+        return parseInt(this.load(this.#INTERNAL_VERSION_KEY), 10);
+    }
+
+    get namespace() {
+        return this.#namespace;
+    }
+
+    get version() {
+        return this.#version;
+    }
+}
